53f89f0cd79a13e930c3e1fefb9c4a5cb7bc9711
Dewmini-Mandis <136433945+Dewmini-Mandis@users.noreply.github.com>
2023-06-24 23:36:17 +0530

    Edit question 8

--- Q8/Q8/src/main/java/com/mycompany/q8/Q8.java
+++ Q8/Q8/src/main/java/com/mycompany/q8/Q8.java
@@ -9,11 +9,17 @@ public class Q8 {
         SavingsAccount saver1 = new SavingsAccount(2000.00f);
         SavingsAccount saver2 = new SavingsAccount(3000.00f);
         SavingsAccount.annualInterestRate=0.04f;
+      
+        saver1.calculateMonthlyInterest();
+        saver2.calculateMonthlyInterest();
         
         System.out.println("Balance of saver1 is "+saver1.getSavingsBalance());
         System.out.println("Balance of saver2 is "+saver2.getSavingsBalance());
         
         SavingsAccount.modifyInterestRate(0.05f);
+        
+        saver1.calculateMonthlyInterest();
+        saver2.calculateMonthlyInterest();
 
         System.out.println("New balance of saver1 is "+saver1.getSavingsBalance());
         System.out.println("New balance of saver2 is "+saver2.getSavingsBalance());
